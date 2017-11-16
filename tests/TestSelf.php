ef03f63c46561b9c10d317b8427f69426d988834
Abax <sclavium@gmail.com>
2017-11-16 21:14:54 +0300

    start

---
+++ tests/TestSelf.php
@@ -0,0 +1,18 @@
+<?php
+require_once "../vendor/autoload.php";
+use PHPUnit\Framework\TestCase;
+/**
+ * Created by PhpStorm.
+ * User: Sergey Pavlov
+ * Date: 16.11.2017
+ * Time: 20:53
+ */
+class TestSelf extends TestCase
+{
+
+    public function test()
+    {
+        echo 'self';
+        $this->assertEquals('1', '1');
+    }
+}
